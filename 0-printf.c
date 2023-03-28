0f153aeb9388ce8e75654844e4aa554da2c87a0c
Simon Muia <simonmuia38@gmail.com>
2023-03-28 22:31:38 +0300

    restructured files

--- 0-printf.c
+++ 0-printf.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
@@ -50,17 +49,4 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -8,10 +8,11 @@
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
 
+void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...);
 int get_flags(const char *format, int *i);
 int get_precision(const char *format, int *i, va_list list);
 int get_width(const char *format, int *i, va_list list);
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+int handle_print( va_list list, char buffer[], int flags, int width, int precision, int size);
 
 #endif
