930f7678c8eed0ece791fb55fe8cce33880f6f64
Simon Muia <simonmuia38@gmail.com>
2023-03-28 20:21:17 +0300

    added struct for print

--- main.h
+++ main.h
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+
 int _printf(const char *format, ...);
-int _putchar(char c);
 
 
 #endif
