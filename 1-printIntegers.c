95697e253c67e4498a0dc3714760bc683d3c634b
Simon Muia <simonmuia38@gmail.com>
2023-03-28 20:27:27 +0300

    print integers

--- 1-printIntegers.c
+++ 1-printIntegers.c
@@ -1,40 +1,44 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-/**
- *
- *
- */
-int print_int()
-{
-	int num = va_arg(args, int);
-	int temp, digits, i;
 
-	if (num < 0)
+/**
+ * print_int - prints an integer
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int print_int(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input;
+
+	unsigned int int_in, int_temp, i, div, isneg;
+
+	int_input = va_arg(arguments, int);
+	isneg = 0;
+	
+	if (int_input < 0)
 	{
-		putchar('-');
-		print_count++;
-		num = -num;
+		int_in = int_input * -1;
+		ibuf = handl_buf(buf, '-', ibuf);
+		isneg = 1;
 	}
-	temp = num;
-	if (temp == 0)
+	else
 	{
-		putchar('0');
-		print_count++;
-		return (print_count);
+		int_in = int_input;
 	}
-	digits = 0;
-	while (temp / 10)
+
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
 	{
-		digits++;
-		temp /= 10;
+		div *= 10;
+		int_temp /= 10;
 	}
-	for (i = digits - 1; i >= 0; i--)
+
+	for (i = 0; div > 0; div /= 10, i++)
 	{
-		int digit = num / pow(10, i);
-		putchar(digit + '0');
-		print_count++;
-		num -= digit * pow(10, i);
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
 	}
+	return (i + isneg);
 }
